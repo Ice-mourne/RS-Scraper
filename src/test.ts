195b33715b15f3827df85559ced9fe5b22f7a7e2
Icemourne <cboweris@gmail.com>
2023-06-30 21:36:06 +0300

    update

--- src/test.ts
+++ src/test.ts
@@ -1,41 +1,10 @@
-import cliProgress from 'cli-progress'
+import fs from 'fs'
+import { Item } from './types.js'
 
-// create new progress bar
-// const b1 = new cliProgress.SingleBar({
-//   format: 'Scraping Item {bar} {value}/{total} {percentage}%',
-//   barCompleteChar: '\u2588',
-//   barIncompleteChar: '\u2591',
-//   hideCursor: true,
-// })
+const items = JSON.parse(fs.readFileSync(`./data/items.json`, 'utf8')) as { [key: string]: Item }
 
-// // initialize the bar - defining payload token "speed" with the default value "N/A"
-// b1.start(10000, 0)
+const data = Object.values(items).map((item) => {
+  return [item.id, item.name, item.memberItem]
+})
 
-// // update values
-// // b1.increment()
-// // b1.update(20)
-
-// // stop the bar
-// b1.stop()
-
-// create new container
-const multiBar = new cliProgress.MultiBar(
-  {
-    clearOnComplete: false,
-    hideCursor: true,
-    format: '{message} {bar} {value}/{total} {percentage}%',
-  },
-  cliProgress.Presets.shades_grey
-)
-
-// add bars
-const b1 = multiBar.create(200, 0)
-const b2 = multiBar.create(1000, 0)
-
-// control bars
-b1.increment()
-b2.update(20, { message: 'Scraping Items' })
-b1.update(20, { message: 'Proxies Left  ' })
-
-// stop all bars
-multiBar.stop()
+fs.writeFileSync(`./data/searchData.json`, JSON.stringify(data))
